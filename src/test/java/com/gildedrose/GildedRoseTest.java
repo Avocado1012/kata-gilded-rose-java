f623d59fab1cacded39e6d41267eb241a600e79e
Lukas <keydon@gmail.com>
2022-06-28 05:35:58 +0000

    pre golden master

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,8 +3,6 @@ package com.gildedrose;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import org.junit.Test;
 
@@ -12,8 +10,8 @@ public class GildedRoseTest {
 
     @Test
     public void executeGoldenMasterTest() throws IOException {
-        String originalOutput = Files.readString(Paths.get("goldenMasterTemplate.txt"));
-        String actualOutput = TexttestFixture.executeGoldenMasterTest(new String[0]);
+        String originalOutput = "NOT IMPLEMENTED";
+        String actualOutput = "TODO";
         assertEquals(originalOutput, actualOutput);
     }
 
